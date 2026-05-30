dfd673fb7128ec0601a1599f6935eb02b3973247
KshSiaan <kshsiaan@gmail.com>
2026-05-31 02:37:35 +0600

    firsts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push(
+        "discord.js",
+        "discord.js/rest",
+        "@discordjs/rest",
+        "@discordjs/ws",
+        "zlib-sync",
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
