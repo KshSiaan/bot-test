093e7f0671910bf28e41380acf754b1be71f2e5c
KshSiaan <kshsiaan@gmail.com>
2026-05-31 02:02:00 +0600

    first

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -1,13 +1,15 @@
 import { Chat } from "chat";
 import { createDiscordAdapter } from "@chat-adapter/discord";
 import { createRedisState } from "@chat-adapter/state-redis";
-const bot = new Chat({
+
+export const bot = new Chat({
   userName: "bot1510288159739744389",
   adapters: {
     discord: createDiscordAdapter(),
   },
   state: createRedisState(),
 });
+
 bot.onNewMention(async (thread, message) => {
   await thread.post("Hello from Discord!");
 });
