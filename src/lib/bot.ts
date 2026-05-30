78d940109f8df2af5876e7fb1a9378466b8b9dd5
KshSiaan <kshsiaan@gmail.com>
2026-05-31 01:45:23 +0600

    first

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -2,7 +2,7 @@ import { Chat } from "chat";
 import { createDiscordAdapter } from "@chat-adapter/discord";
 import { createRedisState } from "@chat-adapter/state-redis";
 const bot = new Chat({
-  userName: "Shiro",
+  userName: "bot1510288159739744389",
   adapters: {
     discord: createDiscordAdapter(),
   },
